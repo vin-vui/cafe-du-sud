fc176fc7a25b49a344d4554d497dce9d1cd165d0
Tekz00r <elderscry@gmail.com>
2023-09-20 13:38:57 +0200

    Model Tag
    Created the model, migration and seeder for Tag
    Created the pivot table and seeder for articles_tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Tag;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TagController extends Controller
+{
+    public function index(){
+    }
+
+    public function show(){
+    }
+
+    public function destroy(Tag $tag){
+    }
+
+    public function store(Request $request){
+    }
+
+    public function update(Request $request, Tag $tag){
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ArticleSeeder::class,
+            TagSeeder::class,
+            ArticleTagSeeder::class,
         ]);
     }
 }
